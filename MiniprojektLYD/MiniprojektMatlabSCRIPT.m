264013cbe8416824d2e7921b352688c866e8f1c9
Lakrids7 <Krelle757@gmail.com>
2023-05-09 11:18:54 +0200

    Added files

---
+++ MiniprojektLYD/MiniprojektMatlabSCRIPT.m
@@ -0,0 +1,15 @@
+D = 20; %The echo delay
+TremoloVar = 400;
+sample_frequency = 1/44000;
+
+
+
+
+%device = mididevice('MPK mini 3')
+
+%simplesynth('MPK mini 3');
+
+
+%msgs = midireceive(device)
+
+%freq = 440 * 2^((mscgs.Note-69)/12);
